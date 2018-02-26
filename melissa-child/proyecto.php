5b77b708b4fe4d43f0da0f1fdbfbe594df888480
padilla0 <padilla.morani@gmail.com>
2018-02-26 14:49:50 +0100

    actualizacion de jose

---
+++ melissa-child/proyecto.php
@@ -0,0 +1,55 @@
+<?php
+
+/*Template Name: Proyecto */
+
+?>
+
+
+
+<!DOCTYPE html>
+
+<html <?php language_attributes(); ?>>
+
+<head>
+
+    
+
+</head>
+
+
+
+<body >
+    <div class="fullscreen-bg">
+        <video loop muted autoplay class="fullscreen-bg__video">
+            <source src="https://proyectoexiste.es/wp-content/uploads/2018/02/Lighthouse-1917.mp4" type="video/mp4">
+        </video>
+    </div>
+
+    <div id="container">
+
+		<div class="row">
+			<div class="col-lg-12">
+
+				<p dir="ltr" style="text-align: center;">¿Podemos hacer una película?</p>
+				<p dir="ltr" style="text-align: center;">Hace un año, una canción nos motivó tanto que nos hizo pensar. ¿Y si hacemos un proyecto audiovisual? No somos directores de cine, ni especialistas ni tenemos años de experiencia. Somos un grupo de jóvenes que queremos hacer algo grande: cambiar el mundo.<b> </b></p>
+				<p dir="ltr" style="text-align: center;">Y #Existe es cambiar el mundo. Es demostrar que la valentía, la felicidad, la alegría, la ilusión, la esperanza, la bondad… existen. Un proyecto audiovisual que quiere lanzar un mensaje: la vida es mucho más, este mundo puede cambiarse.<b> </b></p>
+				<p dir="ltr" style="text-align: center;">Un grupo de jóvenes que ha decidido no conformarse con lo que se supone que ‘tenemos’ que hacer, y apostamos por hacer lo que debemos hacer de verdad. No somos profesionales, ni sabemos cómo se hace una película… pero somos jóvenes y rebeldes. Lo hacemos por los demás. Porque merece la pena.<b> </b></p>
+				<p dir="ltr" style="text-align: center;">Contar historias a través de un largometraje. Historias reales, de gente real… que hacen cosas reales. Historias que nos dicen que se puede ser feliz sin comprar lo que nos venden los anuncios de publicidad.</p>
+				<p dir="ltr" style="text-align: center;">Se puede vivir de verdad, ser feliz de verdad, amar de verdad… Solo hay que lanzarse sin miedo al vacío.</p>				
+
+			</div>
+
+		</div><!-- .row -->
+
+
+
+	</div><!-- .container -->
+</body>
+
+
+
+
+
+		
+
+
